754a2e1e2bbde02d723eec2aac56f47901a35a9c
Andy Le <andylequ@gmail.com>
2025-01-17 17:20:36 -0600

    v1

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,25 @@
 // build your `Resource` model here
+//  - [ ] `resource_id` - primary key
+//   - [ ] `resource_name` - required and unique
+//   - [ ] `resource_description` - optional
+
+const db = require('../../data/dbConfig');
+
+module.exports = {
+    get,
+    insert,
+}
+
+function get() {
+    return db('resources')
+}
+
+function insert(resource) {
+    return db('resources')
+    .insert(resource)
+    .then(([resource_id]) => {
+        return db('resources')
+        .where('resource_id', resource_id)
+        .first()
+    })
+}
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,22 @@
 // build your `/api/resources` router here
+const express = require('express');
+const router = express.Router();
+const Resource = require('./model');
+
+router.get('/', (req, res, next) => {
+    Resource.get()
+    .then(resources => {
+        res.json(resources)
+    })
+    .catch(next)
+})
+
+router.post('/', (req, res, next) => {
+    Resource.insert(req.body)
+    .then(resource => {
+        res.status(201).json(resource)
+    })
+    .catch(next)
+})
+
+module.exports = router;
